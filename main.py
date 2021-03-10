d2d6ed76d9fdf3f912d2defa820d8b9d43a63e05
KrzysiekWojcik22 <krzysztofwojcik@tuta.io>
2021-03-10 13:03:28 +0100

    Initial commit v2

--- main.py
+++ main.py
@@ -35,18 +35,37 @@ class Application(tk.Frame):
         self.AddComp = tk.Button(root, text="Quit", command=self.master.destroy)
         self.AddComp.place(height=40, width=80, x=950, y=665)
 
+        self.AddComp = tk.Button(root, text="+", command=self.master.destroy)
+        self.AddComp.place(height=40, width=40, x=265, y=240)
+
+        self.AddComp = tk.Button(root, text="-", command=self.master.destroy)
+        self.AddComp.place(height=40, width=40, x=214, y=240)
+
         self.Zastepcze = tk.Button(root, text="Tutaj Obrazek", command=self.say_hi)
         self.Zastepcze.place(height=400, width=400, x=630, y=15)
 
+        self.AddComp = tk.Button(root, text="Export1", command=self.say_hi)
+        self.AddComp.place(height=40, width=80, x=15, y=500)
+
+        self.AddComp = tk.Button(root, text="Export2", command=self.say_hi)
+        self.AddComp.place(height=40, width=80, x=110, y=500)
+
+        self.AddComp = tk.Button(root, text="Export3", command=self.say_hi)
+        self.AddComp.place(height=40, width=80, x=205, y=500)
+
+
 
         self.Kategoria = tk.Entry(root, width=50)
-        self.Kategoria.place(height=25, width=100, x=240, y=50)
+        self.Kategoria.place(height=25, width=100, x=210, y=50)
 
         self.Typ = tk.Entry(root, width=50)
-        self.Typ.place(height=25, width=100, x=200, y=570)
+        self.Typ.place(height=25, width=100, x=210, y=100)
 
         self.Ilosc = tk.Entry(root, width=50)
-        self.Ilosc.place(height=25, width=100, x=200, y=570)
+        self.Ilosc.place(height=25, width=100, x=210, y=150)
+
+        self.Where = tk.Entry(root, width=50)
+        self.Where.place(height=25, width=100, x=210, y=200)
 
         self.Informacje=tk.Label(root, text="Information:")
         self.Informacje.place(height=40, width=80, x=350, y=10)
@@ -58,15 +77,17 @@ class Application(tk.Frame):
         self.Informacje.place(height=40, width=80, x=120, y=40)
 
         self.Informacje = tk.Label(root, text="Type:", )
-        self.Informacje.place(height=40, width=80, x=120, y=70)
-
-        self.Informacje = tk.Label(root, text="Quantity:", )
-        self.Informacje.place(height=40, width=80, x=120, y=100)
+        self.Informacje.place(height=40, width=80, x=120, y=90)
 
         self.Informacje = tk.Label(root, text="Where:", )
-        self.Informacje.place(height=40, width=80, x=120, y=100)
+        self.Informacje.place(height=40, width=80, x=120, y=140)
+
+        self.Informacje = tk.Label(root, text="Quantity:", )
+        self.Informacje.place(height=40, width=80, x=120, y=190)
 
 
+        self.Informacje = tk.Label(root, text="text" )
+        self.Informacje.place(height=40, width=80, x=500, y=190)
 
     def say_hi(self):
         print("hi there, everyone!")
