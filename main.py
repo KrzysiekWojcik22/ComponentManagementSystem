6416b51ca6d365770c1990901b8595d2ab4e9430
KrzysiekWojcik22 <krzysztofwojcik@tuta.io>
2021-03-17 14:07:58 +0100

    v1.1 sql

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from tkinter import messagebox
 import mysql.connector
 
 
-
-
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -18,14 +16,8 @@ class Application(tk.Frame):
         top.title('Add new equipment')
         top.geometry("300x500")
 
-
-
-
-
-
-
-        self.AddComponent = tk.Button(top, text="Add EQ",command=self.fAddComponent)
-        self.AddComponent.place(height=40,width=80, x =15, y=445 , )
+        self.AddComponent = tk.Button(top, text="Add EQ", command=self.fAddComponent)
+        self.AddComponent.place(height=40, width=80, x=15, y=445, )
 
         self.ClearComponent = tk.Button(top, text="Clear", command=self.fClearComponent)
         self.ClearComponent.place(height=40, width=80, x=110, y=445, )
@@ -57,40 +49,84 @@ class Application(tk.Frame):
         self.lQuantity = tk.Label(top, text="Quantity:", )
         self.lQuantity.place(height=40, width=80, x=10, y=360)
 
-        SizeComponents = ["0201","0402", "0603","0805","1008","1206","1210"]
-        Categories = ["Semiconductor",]
+        SizeComponents = ["0201", "0402", "0603", "0805", "1008", "1206", "1210"]
+        Categories = ["Semiconductor", ]
         SposobMontazu = ["THT", "SMD"]
-        #CasesTHT = ["DIP", "SDIP" , "TO" ]
+        # CasesTHT = ["DIP", "SDIP" , "TO" ]
         CasesSMD = ["SOJ", "SOIC", "PLCC", "QFP", "BGA"]
         grupa = ["1"]
 
+        self.eName = tk.Entry(top, width=50)
+        self.eName.place(height=20, width=180, x=100, y=20)
 
-        self.Name = tk.Entry(top, width=50)
-        self.Name.place(height=20, width=180, x=100, y=20)
+        self.eCategory = ttk.Combobox(top, values=Categories)
+        self.eCategory.place(height=20, width=180, x=100, y=70)
 
-        self.KategoriaZW = ttk.Combobox(top, values = Categories)
-        self.KategoriaZW.place(height=20, width=180, x=100, y=70)
+        self.eGroup = ttk.Combobox(top, values=grupa)
+        self.eGroup.place(height=20, width=180, x=100, y=120)
 
-        self.Grupaz = ttk.Combobox(top, values = grupa)
-        self.Grupaz.place(height=20, width=180, x=100, y=120)
+        self.eAssembly = ttk.Combobox(top, values=SposobMontazu)
+        self.eAssembly.place(height=20, width=180, x=100, y=170)
 
-        self.Montazz = ttk.Combobox(top, values = SposobMontazu)
-        self.Montazz.place(height=20, width=180, x=100, y=170)
+        self.eCase = ttk.Combobox(top, values=CasesSMD)
+        self.eCase.place(height=20, width=180, x=100, y=220)
 
-        self.Obudowaz = ttk.Combobox(top, values = CasesSMD)
-        self.Obudowaz.place(height=20, width=180, x=100, y=220)
+        self.eSize = ttk.Combobox(top, values=SizeComponents)
+        self.eSize.place(height=20, width=180, x=100, y=270)
 
-        self.rozmiarz = ttk.Combobox(top, values = SizeComponents)
-        self.rozmiarz.place(height=20, width=180, x=100, y=270)
+        self.eStorage = ttk.Combobox(top, values=CasesSMD)
+        self.eStorage.place(height=20, width=180, x=100, y=320)
 
-        self.rozmiarz = ttk.Combobox(top, values=CasesSMD)
-        self.rozmiarz.place(height=20, width=180, x=100, y=320)
+        self.eQuantity = tk.Entry(top, width=50)
+        self.eQuantity.place(height=20, width=180, x=100, y=370)
+
+    def ShowListOfComponents(self):
+        top2 = tk.Toplevel()
+        top2.title('List Of Components')
+        top2.geometry("800x500")
+
+        mydb = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='KrzysiekmySql12',
+            database="testsql"
+        )
+
+        cursor = mydb.cursor()
+        cursor.execute("Select * from components   ")
+        rows = cursor.fetchall()
+        scrollbar = tk.Scrollbar(top2, orient="vertical")
+        list = tk.Listbox(top2, width=90, height=28)
+
+        list.place(x=15, y=15)
+
+        for row in rows:
+            insertData = (str(row[0]) + '    ' + row[1] + '    ' + row[2] + '    ' + row[3] +
+                          '    ' + row[4] + '    ' + row[5] + '    ' + row[6] + ' ' + str(row[7]))
+            list.insert(list.size() + 1, insertData)
+        mydb.close()
+
+    def fDeleteComponent(self):
 
 
+        Id = Id.get()
+        if (Id == ""):
+            messagebox.showerror("Delete Status", "This Id is no exist: ")
+        else:
+            mydb = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='KrzysiekmySql12',
+            database="testsql"
+            )
 
-        self.iloscz = tk.Entry(top, width=50)
-        self.iloscz.place(height=20, width=180, x=100, y=370)
 
+        cursor = mydb.cursor()
+        cursor.execute("delete from ")
+        messagebox.showinfo("Insert Status", "Component was added")
+        mydb.close()
+
+        self.Name.delete(0, 'end')
 
 
 
@@ -106,14 +142,12 @@ class Application(tk.Frame):
         top = tk.Toplevel()
         top.title('Delete equipment')
 
-
-
     def create_widgets(self):
 
         self.AddComp = tk.Button(root, text="Add new Eq", command=self.Add)
         self.AddComp.place(height=40, width=80, x=15, y=15)
 
-        self.ShowComp = tk.Button(root, text="Show list", command=self.show)
+        self.ShowComp = tk.Button(root, text="Show list", command=self.ShowListOfComponents)
         self.ShowComp.place(height=40, width=80, x=15, y=65)
 
         self.FindComp = tk.Button(root, text="Find", command=self.find)
@@ -152,8 +186,6 @@ class Application(tk.Frame):
         self.AddComp = tk.Button(root, text="Export3", command=self.say_hi)
         self.AddComp.place(height=40, width=80, x=205, y=500)
 
-
-
         self.Kategoria = tk.Entry(root, width=50)
         self.Kategoria.place(height=25, width=100, x=210, y=50)
 
@@ -184,8 +216,7 @@ class Application(tk.Frame):
         self.Informacje = tk.Label(root, text="Quantity:", )
         self.Informacje.place(height=40, width=80, x=120, y=190)
 
-
-        self.Informacje = tk.Label(root, text="text" )
+        self.Informacje = tk.Label(root, text="text")
         self.Informacje.place(height=40, width=80, x=500, y=190)
 
     def say_hi(self):
@@ -195,13 +226,18 @@ class Application(tk.Frame):
         print("show list")
 
     def fAddComponent(self):
-        print("AddComponent")
-        #messagebox.showinfo("Information", "Components was added")
 
+        name = self.eName.get()
+        category = self.eCategory.get()
+        group = str(self.eGroup.get())
+        assembly = self.eAssembly.get()
+        size = self.eSize.get()
+        case=self.eCase.get()
+        storage = self.eStorage.get()
+        quantity = self.eQuantity.get()
 
-
-        name = self.Name.get()
-        if (name == ""):
+        if (
+                name == "" or category == "" or group == "" or assembly == "" or size == "" or storage == "" or quantity == "" or case==""):
             messagebox.showerror("Error", "All Fields are required")
         else:
             mydb = mysql.connector.connect(
@@ -211,64 +247,61 @@ class Application(tk.Frame):
                 database="testsql"
             )
             cursor = mydb.cursor()
-            cursor.execute("")
+            cursor.execute(
+                "insert Into components(Name_db,Category_db,Group_db,Assembly_db,Case_db,Size_db,Storage_db,Quantity_db) Values ('" + name + "', '" + category + "','" + group + "','" + assembly + "','" + case + "', '" + size + "', '" + storage + "', '" + quantity + "')")
+
+            cursor.execute("commit")
             messagebox.showinfo("Insert Status", "Component was added")
             mydb.close()
 
-            self.Name.delete(0,'end')
-
+            self.eName.delete(0, 'end')
+            self.eCategory.delete(0, 'end')
+            self.eGroup.delete(0, 'end')
+            self.eAssembly.delete(0, 'end')
+            self.eSize.delete(0, 'end')
+            self.eCase.delete(0,'end')
+            self.eStorage.delete(0, 'end')
+            self.eQuantity.delete(0, 'end')
 
     def fClearComponent(self):
-        print("ClearComponent")
-'''
-    def.fDeleteComponent(self):
-        Id = Id.get()
-        if (Id==""):
-            messagebox.showerror("Delete Status", "This Id is no exist: ")
-        else:
-        mydb = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='KrzysiekmySql12',
-            database="testsql"
-        )
+        self.eName.delete(0, 'end')
+        self.eCategory.delete(0, 'end')
+        self.eGroup.delete(0, 'end')
+        self.eAssembly.delete(0, 'end')
+        self.eSize.delete(0, 'end')
+        self.eCase.delete(0, 'end')
+        self.eStorage.delete(0, 'end')
+        self.eQuantity.delete(0, 'end')
 
-
-cursor = mydb.cursor()
-cursor.execute("delete from ")
-messagebox.showinfo("Insert Status", "Component was added")
-mydb.close()
-
-self.Name.delete(0, 'end')
-'''
-
-
-'''
-    def fUpdateComponent(self):
-
+    '''
+        
+    
 
     
-        def.fDeleteComponent(self):
-            Id = Id.get()
-            if (Id==""):
-                messagebox.showerror("Delete Status", "This Id is no exist: ")
-            else:
-            mydb = mysql.connector.connect(
-                host='localhost',
-                user='root',
-                password='KrzysiekmySql12',
-                database="testsql"
-            )
+        def fUpdateComponent(self):
 
 
-    cursor = mydb.cursor()
-    cursor.execute("delete from ")
-    messagebox.showinfo("Insert Status", "Component was added")
-    mydb.close()
 
-    self.Name.delete(0, 'end')
-    '''
+            def.fDeleteComponent(self):
+                Id = Id.get()
+                if (Id==""):
+                    messagebox.showerror("Delete Status", "This Id is no exist: ")
+                else:
+                mydb = mysql.connector.connect(
+                    host='localhost',
+                    user='root',
+                    password='KrzysiekmySql12',
+                    database="testsql"
+                )
 
+
+        cursor = mydb.cursor()
+        cursor.execute("delete from ")
+        messagebox.showinfo("Insert Status", "Component was added")
+        mydb.close()
+
+        self.Name.delete(0, 'end')
+        '''
 
     def fQuitComponent(self):
         print("Quit")
