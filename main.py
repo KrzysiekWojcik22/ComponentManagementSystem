7b3484f1db52bd6f0015619bc6093b32de577b41
KrzysiekWojcik22 <krzysztofwojcik@tuta.io>
2021-03-18 15:40:54 +0100

    v1.2 sql

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 import mysql.connector
 
 
-class Application(tk.Frame):
+class Application(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
@@ -16,37 +16,37 @@ class Application(tk.Frame):
         top.title('Add new equipment')
         top.geometry("300x500")
 
-        self.AddComponent = tk.Button(top, text="Add EQ", command=self.fAddComponent)
+        self.AddComponent = ttk.Button(top, text="Add EQ", command=self.fAddComponent)
         self.AddComponent.place(height=40, width=80, x=15, y=445, )
 
-        self.ClearComponent = tk.Button(top, text="Clear", command=self.fClearComponent)
+        self.ClearComponent = ttk.Button(top, text="Clear", command=self.fClearComponent)
         self.ClearComponent.place(height=40, width=80, x=110, y=445, )
 
-        self.QuitComponent = tk.Button(top, text="Quit", command=self.fQuitComponent)
+        self.QuitComponent = ttk.Button(top, text="Quit", command=self.fQuitComponent)
         self.QuitComponent.place(height=40, width=80, x=205, y=445, )
 
-        self.lName = tk.Label(top, text="Name:", )
+        self.lName = ttk.Label(top, text="Name:", )
         self.lName.place(height=40, width=80, x=10, y=10)
 
-        self.lCategory = tk.Label(top, text="Category:", )
+        self.lCategory = ttk.Label(top, text="Category:", )
         self.lCategory.place(height=40, width=80, x=10, y=60)
 
-        self.lGroup = tk.Label(top, text="Group:", )
+        self.lGroup = ttk.Label(top, text="Group:", )
         self.lGroup.place(height=40, width=80, x=10, y=110)
 
-        self.lAssembly = tk.Label(top, text="Assembly:", )
+        self.lAssembly = ttk.Label(top, text="Assembly:", )
         self.lAssembly.place(height=40, width=80, x=10, y=160)
 
-        self.lCase = tk.Label(top, text="Case:", )
+        self.lCase = ttk.Label(top, text="Case:", )
         self.lCase.place(height=40, width=80, x=10, y=210)
 
-        self.lSize = tk.Label(top, text="Size:", )
+        self.lSize = ttk.Label(top, text="Size:", )
         self.lSize.place(height=40, width=80, x=10, y=260)
 
-        self.lStorage = tk.Label(top, text="Storage:", )
+        self.lStorage = ttk.Label(top, text="Storage:", )
         self.lStorage.place(height=40, width=80, x=10, y=310)
 
-        self.lQuantity = tk.Label(top, text="Quantity:", )
+        self.lQuantity = ttk.Label(top, text="Quantity:", )
         self.lQuantity.place(height=40, width=80, x=10, y=360)
 
         SizeComponents = ["0201", "0402", "0603", "0805", "1008", "1206", "1210"]
@@ -56,7 +56,7 @@ class Application(tk.Frame):
         CasesSMD = ["SOJ", "SOIC", "PLCC", "QFP", "BGA"]
         grupa = ["1"]
 
-        self.eName = tk.Entry(top, width=50)
+        self.eName = ttk.Entry(top, width=50)
         self.eName.place(height=20, width=180, x=100, y=20)
 
         self.eCategory = ttk.Combobox(top, values=Categories)
@@ -77,7 +77,7 @@ class Application(tk.Frame):
         self.eStorage = ttk.Combobox(top, values=CasesSMD)
         self.eStorage.place(height=20, width=180, x=100, y=320)
 
-        self.eQuantity = tk.Entry(top, width=50)
+        self.eQuantity = ttk.Entry(top, width=50)
         self.eQuantity.place(height=20, width=180, x=100, y=370)
 
     def ShowListOfComponents(self):
@@ -106,6 +106,7 @@ class Application(tk.Frame):
             list.insert(list.size() + 1, insertData)
         mydb.close()
 
+    '''
     def fDeleteComponent(self):
 
 
@@ -128,7 +129,7 @@ class Application(tk.Frame):
 
         self.Name.delete(0, 'end')
 
-
+'''
 
     def show(self):
         top = tk.Toplevel()
@@ -144,79 +145,79 @@ class Application(tk.Frame):
 
     def create_widgets(self):
 
-        self.AddComp = tk.Button(root, text="Add new Eq", command=self.Add)
+        self.AddComp = ttk.Button(root, text="Add new Eq", command=self.Add)
         self.AddComp.place(height=40, width=80, x=15, y=15)
 
-        self.ShowComp = tk.Button(root, text="Show list", command=self.ShowListOfComponents)
+        self.ShowComp = ttk.Button(root, text="Show list", command=self.ShowListOfComponents)
         self.ShowComp.place(height=40, width=80, x=15, y=65)
 
-        self.FindComp = tk.Button(root, text="Find", command=self.find)
+        self.FindComp = ttk.Button(root, text="Find", command=self.find)
         self.FindComp.place(height=40, width=80, x=15, y=115)
 
-        self.DeleteComp = tk.Button(root, text="Delete Eq", command=self.delete)
+        self.DeleteComp = ttk.Button(root, text="Delete Eq", command=self.delete)
         self.DeleteComp.place(height=40, width=80, x=15, y=165)
 
-        self.AddComp = tk.Button(root, text="<", command=self.say_hi)
+        self.AddComp = ttk.Button(root, text="<", command=self.say_hi)
         self.AddComp.place(height=40, width=80, x=700, y=430)
 
-        self.AddComp = tk.Button(root, text=">", command=self.say_hi)
+        self.AddComp = ttk.Button(root, text=">", command=self.say_hi)
         self.AddComp.place(height=40, width=80, x=880, y=430)
 
-        self.AddComp = tk.Button(root, text="INFO", command=self.say_hi)
+        self.AddComp = ttk.Button(root, text="INFO", command=self.say_hi)
         self.AddComp.place(height=40, width=80, x=790, y=430)
 
-        self.AddComp = tk.Button(root, text="Quit", command=self.master.destroy)
+        self.AddComp = ttk.Button(root, text="Quit", command=self.master.destroy)
         self.AddComp.place(height=40, width=80, x=950, y=665)
 
-        self.AddComp = tk.Button(root, text="+", command=self.master.destroy)
+        self.AddComp = ttk.Button(root, text="+", command=self.master.destroy)
         self.AddComp.place(height=40, width=40, x=265, y=240)
 
-        self.AddComp = tk.Button(root, text="-", command=self.master.destroy)
+        self.AddComp = ttk.Button(root, text="-", command=self.master.destroy)
         self.AddComp.place(height=40, width=40, x=214, y=240)
 
-        self.Zastepcze = tk.Button(root, text="Tutaj Obrazek", command=self.say_hi)
+        self.Zastepcze = ttk.Button(root, text="Tutaj Obrazek", command=self.say_hi)
         self.Zastepcze.place(height=400, width=400, x=630, y=15)
 
-        self.AddComp = tk.Button(root, text="Export1", command=self.say_hi)
+        self.AddComp = ttk.Button(root, text="Export1", command=self.say_hi)
         self.AddComp.place(height=40, width=80, x=15, y=500)
 
-        self.AddComp = tk.Button(root, text="Export2", command=self.say_hi)
+        self.AddComp = ttk.Button(root, text="Export2", command=self.say_hi)
         self.AddComp.place(height=40, width=80, x=110, y=500)
 
-        self.AddComp = tk.Button(root, text="Export3", command=self.say_hi)
+        self.AddComp = ttk.Button(root, text="Export3", command=self.say_hi)
         self.AddComp.place(height=40, width=80, x=205, y=500)
 
-        self.Kategoria = tk.Entry(root, width=50)
+        self.Kategoria = ttk.Entry(root, width=50)
         self.Kategoria.place(height=25, width=100, x=210, y=50)
 
-        self.Typ = tk.Entry(root, width=50)
+        self.Typ = ttk.Entry(root, width=50)
         self.Typ.place(height=25, width=100, x=210, y=100)
 
-        self.Ilosc = tk.Entry(root, width=50)
+        self.Ilosc = ttk.Entry(root, width=50)
         self.Ilosc.place(height=25, width=100, x=210, y=150)
 
-        self.Where = tk.Entry(root, width=50)
+        self.Where = ttk.Entry(root, width=50)
         self.Where.place(height=25, width=100, x=210, y=200)
 
-        self.Informacje = tk.Label(root, text="Information:")
+        self.Informacje = ttk.Label(root, text="Information:")
         self.Informacje.place(height=40, width=80, x=350, y=10)
 
-        self.Informacje = tk.Label(root, text="Fast Find:", )
+        self.Informacje = ttk.Label(root, text="Fast Find:", )
         self.Informacje.place(height=40, width=80, x=120, y=10)
 
-        self.Informacje = tk.Label(root, text="Category:", )
+        self.Informacje = ttk.Label(root, text="Category:", )
         self.Informacje.place(height=40, width=80, x=120, y=40)
 
-        self.Informacje = tk.Label(root, text="Type:", )
+        self.Informacje = ttk.Label(root, text="Type:", )
         self.Informacje.place(height=40, width=80, x=120, y=90)
 
-        self.Informacje = tk.Label(root, text="Where:", )
+        self.Informacje = ttk.Label(root, text="Where:", )
         self.Informacje.place(height=40, width=80, x=120, y=140)
 
         self.Informacje = tk.Label(root, text="Quantity:", )
         self.Informacje.place(height=40, width=80, x=120, y=190)
 
-        self.Informacje = tk.Label(root, text="text")
+        self.Informacje = ttk.Label(root, text="text")
         self.Informacje.place(height=40, width=80, x=500, y=190)
 
     def say_hi(self):
